e131c585cd1a010de9053a87daf0d1d4c08c0adf
GauravChy09 <gauravgkchy139@gmail.com>
2025-03-15 02:51:23 +0530

    Added front-end part made in react js

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css'
+import Authentication from './pages/Authentication/authentication';
+
+function App() {
+  return (
+    <div className="">
+      <Grid container>
+        <Grid item xs={7}>
+          <img classname='h-full w-full' src='https://previews.123rf.com/images/natpra/natpra2008/natpra200800003/152609304-people-group-talking-together-on-video-conference-app-phone-call-video-chat-with-people-on-screen.jpg'></img>
+        </Grid>
+      </Grid>
+    </div>
+  );
+}
+
+export default App;
